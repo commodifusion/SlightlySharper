3b3fa893d8bd328383013d8950df66824f9ce0e2
Duncan Pierce <duncan.pierce@commodifusion.com>
2012-04-30 13:19:03 +0100

    Created solution and added IDEA-like keybinding

---
+++ AssemblyInfo.cs
@@ -0,0 +1,27 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+
+// Information about this assembly is defined by the following attributes. 
+// Change them to the values specific to your project.
+
+[assembly: AssemblyTitle("SlightlySharper")]
+[assembly: AssemblyDescription("A keybinding add-in to make MonoDevelop behave slightly more like JetBrains Resharper or IDEA")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CommodiFusion Ltd")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("Copyright CommodiFusion Ltd 2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// The assembly version has the format "{Major}.{Minor}.{Build}.{Revision}".
+// The form "{Major}.{Minor}.*" will automatically update the build and revision,
+// and "{Major}.{Minor}.{Build}.*" will update just the revision.
+
+[assembly: AssemblyVersion("1.0.*")]
+
+// The following attributes are used to specify the signing key for the assembly, 
+// if desired. See the Mono documentation for more information about signing.
+
+//[assembly: AssemblyDelaySign(false)]
+//[assembly: AssemblyKeyFile("")]
+
---
+++ SlightlySharperAddIn.cs
@@ -0,0 +1,14 @@
+using MonoDevelop.Components.Commands;
+using MonoDevelop.Ide;
+using System;
+
+namespace CommodiFusion.SlightlySharper
+{
+	public class SlightlySharperAddIn : CommandHandler
+	{
+		protected override void Run()
+		{
+			//MessageService.ShowMessage("SlightlySharper", "Starting SlightlySharper add-in");
+		}
+	}
+}
